fabe36563f22af3cb72a7ef0552d6380c4db9be2
Sylvain Marie <sylvain.marie@schneider-electric.com>
2018-07-05 17:38:32 +0200

    Added tests to check that test functions could rely on cases in the same file but could still select them using filters

---
+++ pytest_cases/tests/test_shared_cases.py
@@ -0,0 +1,24 @@
+from pytest_cases.tests.example_code import super_function_i_want_to_test, super_function_i_want_to_test2
+
+from pytest_cases import case_name, CaseData, test_target, case_tags
+
+
+@test_target(super_function_i_want_to_test)
+def case_for_function1() -> CaseData:
+    ins = dict(a=1, b=2)
+    outs = 2, 3
+    return ins, outs, None
+
+
+@test_target(super_function_i_want_to_test2)
+def case_for_function2() -> CaseData:
+    ins = dict(a=1, b=2)
+    outs = 2, 3
+    return ins, outs, None
+
+
+@case_tags(super_function_i_want_to_test, super_function_i_want_to_test2)
+def case_for_function_1_and_2() -> CaseData:
+    ins = dict(a=1, b=2)
+    outs = 2, 3
+    return ins, outs, None
