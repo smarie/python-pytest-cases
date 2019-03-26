6046df06a39bcd47dfc22729b154c7e484e36670
Sylvain MARIE <sylvain.marie@se.com>
2019-03-26 13:16:11 +0100

    Fixed test for pytest 2.x

--- pytest_cases/tests/simple/test_paramfixtures.py
+++ pytest_cases/tests/simple/test_paramfixtures.py
@@ -1,5 +1,16 @@
+from distutils.version import LooseVersion
+
 import pytest
 from pytest_cases import param_fixture, param_fixtures, pytest_fixture_plus
+
+
+# pytest.param - not available in all versions
+if LooseVersion(pytest.__version__) >= LooseVersion('3.0.0'):
+    pytest_param = pytest.param
+else:
+    def pytest_param(*args, **kwargs):
+        return args
+
 
 # create a single parameter fixture
 my_parameter = param_fixture("my_parameter", [1, 2, 3, 4])
@@ -39,8 +50,8 @@ parg1, parg2 = param_fixtures("parg1, parg2", [("a", "b"), ("c", "d")])
 
 @pytest_fixture_plus
 @pytest.mark.parametrize("arg1, arg2", [
-    pytest.param(1, 2, id="f_a"),
-    pytest.param(3, 4, id="f_b")
+    pytest_param(1, 2, id="f_a"),
+    pytest_param(3, 4, id="f_b")
 ])
 def myfix(arg1, arg2, parg1):
     """One parameterized fixture relying on above param fixture"""
@@ -48,15 +59,20 @@ def myfix(arg1, arg2, parg1):
 
 
 @pytest.mark.parametrize("arg3, arg4", [
-    pytest.param(10, 20, id="t_a"),
-    pytest.param(30, 40, id="t_b")
+    pytest_param(10, 20, id="t_a"),
+    pytest_param(30, 40, id="t_b")
 ])
 def test_one(myfix, arg3, arg4, parg1, parg2, request):
     """"""
     assert myfix[2] == parg1
     paramvalues = request.node.nodeid.split('[')[1][:-1]
-    arg1arg2id = "f_a" if myfix[:-1] == (1, 2) else "f_b"
-    arg3arg4id = "t_a" if (arg3, arg4) == (10, 20) else "t_b"
+    if LooseVersion(pytest.__version__) >= LooseVersion('3.0.0'):
+        arg1arg2id = "f_a" if myfix[:-1] == (1, 2) else "f_b"
+        arg3arg4id = "t_a" if (arg3, arg4) == (10, 20) else "t_b"
+    else:
+        arg1arg2id = "-".join(["%s" % v for v in myfix[:-1]])
+        arg3arg4id = "-".join(["%s" % v for v in (arg3, arg4)])
+
     assert paramvalues == "{}-{}-{}-{}".format(arg1arg2id, parg1, parg2, arg3arg4id)
     # print("parg1={} parg2={} myfix={} arg3={} arg4={}".format(parg1, parg2, myfix, arg3, arg4))
 
@@ -64,17 +80,29 @@ def test_one(myfix, arg3, arg4, parg1, parg2, request):
 def test_synthesis(module_results_dct):
     """Use pytest-harvest to check that the list of executed tests is correct """
 
+    if LooseVersion(pytest.__version__) >= LooseVersion('3.0.0'):
+        end_list = ['test_one[f_a-a-b-t_a]',
+                    'test_one[f_a-a-b-t_b]',
+                    'test_one[f_a-c-d-t_a]',
+                    'test_one[f_a-c-d-t_b]',
+                    'test_one[f_b-a-b-t_a]',
+                    'test_one[f_b-a-b-t_b]',
+                    'test_one[f_b-c-d-t_a]',
+                    'test_one[f_b-c-d-t_b]']
+    else:
+        end_list = [ 'test_one[1-2-a-b-10-20]',
+                     'test_one[1-2-a-b-30-40]',
+                     'test_one[1-2-c-d-10-20]',
+                     'test_one[1-2-c-d-30-40]',
+                     'test_one[3-4-a-b-10-20]',
+                     'test_one[3-4-a-b-30-40]',
+                     'test_one[3-4-c-d-10-20]',
+                     'test_one[3-4-c-d-30-40]']
+
     assert list(module_results_dct) == ['test_uses_param[1]',
                                         'test_uses_param[2]',
                                         'test_uses_param[3]',
                                         'test_uses_param[4]',
                                         'test_uses_param2[1-2]',
                                         'test_uses_param2[3-4]',
-                                        'test_one[f_a-a-b-t_a]',
-                                        'test_one[f_a-a-b-t_b]',
-                                        'test_one[f_a-c-d-t_a]',
-                                        'test_one[f_a-c-d-t_b]',
-                                        'test_one[f_b-a-b-t_a]',
-                                        'test_one[f_b-a-b-t_b]',
-                                        'test_one[f_b-c-d-t_a]',
-                                        'test_one[f_b-c-d-t_b]']
+                                        ] + end_list
