f6b121db506cdde02981b57a2fcad67e49adec33
Sylvain MARIE <sylvain.marie@se.com>
2019-04-05 21:35:19 +0200

    Updated test to match stack overflow answer

--- pytest_cases/tests/fixtures/test_so.py
+++ pytest_cases/tests/fixtures/test_so.py
@@ -8,9 +8,14 @@ def lower(request):
     return "i" * request.param
 
 
+# @pytest_fixture_plus(params=[1, 2])
+# def upper(request):
+#     return "I" * request.param
+
 @pytest.fixture(params=[1, 2])
 def upper(request):
-    # this fixture does not use pytest_fixture_plus so we have to explicitly discard the 'not used' cases
+    # Just for the remark: this fixture does not use pytest_fixture_plus
+    # so we have to explicitly discard the 'NOT_USED' cases
     if request.param is not NOT_USED:
         return "I" * request.param
 
@@ -20,3 +25,13 @@ fixture_union('all', ['lower', 'upper'])
 
 def test_all(all):
     print(all)
+
+
+def test_synthesis(module_results_dct):
+    """Use pytest-harvest to check that the list of executed tests is correct """
+
+    assert list(module_results_dct) == ['test_all[lower-1]',
+                                        'test_all[lower-2]',
+                                        'test_all[lower-3]',
+                                        'test_all[upper-1]',
+                                        'test_all[upper-2]']
