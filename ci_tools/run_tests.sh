cde1f3d1c301b223704f0274eef9bb82b159c296
Sylvain MARIE <sylvain.marie@se.com>
2019-06-17 16:14:00 +0200

    Trying to fix travis issue with pytest coverage

--- ci_tools/run_tests.sh
+++ ci_tools/run_tests.sh
@@ -16,7 +16,9 @@ if [ "${TRAVIS_PYTHON_VERSION}" = "3.5" ]; then
    # full
    # Run tests with "python -m pytest" to use the correct version of pytest
    echo -e "\n\n****** Running tests ******\n\n"
-   python -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./pytest_cases -v pytest_cases/tests/
+   coverage run --source pytest_cases -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html -v pytest_cases/tests/
+   # buggy
+   # python -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./pytest_cases -v pytest_cases/tests/
 else
    # faster - skip coverage and html report but keep junit (because used in validity threshold)
     echo -e "\n\n****** Running tests******\n\n"
