ea4620e83f4c8cb7bcea9d0266c73bb6b6ef753c
Sylvain MARIE <sylvain.marie@se.com>
2019-12-19 12:47:39 +0100

    Added a global fixture in a test

--- pytest_cases/tests/fixtures/test_fixture_in_parametrize_basic.py
+++ pytest_cases/tests/fixtures/test_fixture_in_parametrize_basic.py
@@ -1,5 +1,6 @@
 import pytest
 from pytest_cases import pytest_parametrize_plus, pytest_fixture_plus, fixture_ref
+from pytest_cases.tests.conftest import global_fixture
 
 
 @pytest.fixture
@@ -13,7 +14,8 @@ def greetings(who):
     return 'hello ' + who
 
 
-@pytest_parametrize_plus('main_msg', ['nothing', fixture_ref(world_str), fixture_ref(greetings)])
+@pytest_parametrize_plus('main_msg', ['nothing', fixture_ref(world_str), fixture_ref(greetings),
+                                      fixture_ref(global_fixture)])
 @pytest.mark.parametrize('ending', ['?', '!'])
 def test_prints(main_msg, ending):
     print(main_msg + ending)
@@ -27,4 +29,6 @@ def test_synthesis(module_results_dct):
                                         'test_prints[main_msg_is_greetings-who_is_world_str-?]',
                                         'test_prints[main_msg_is_greetings-who_is_world_str-!]',
                                         'test_prints[main_msg_is_greetings-who_is_1-you-?]',
-                                        'test_prints[main_msg_is_greetings-who_is_1-you-!]']
+                                        'test_prints[main_msg_is_greetings-who_is_1-you-!]',
+                                        'test_prints[main_msg_is_global_fixture-?]',
+                                        'test_prints[main_msg_is_global_fixture-!]']
