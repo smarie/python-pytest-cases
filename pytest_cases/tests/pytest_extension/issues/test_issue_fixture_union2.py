39922cfa1e7c11bb86187340e320fe38ee0eec87
Sylvain MARIE <sylvain.marie@se.com>
2020-07-28 16:22:47 +0200

    Improved test: added with/without ids variant

--- pytest_cases/tests/pytest_extension/issues/test_issue_fixture_union2.py
+++ pytest_cases/tests/pytest_extension/issues/test_issue_fixture_union2.py
@@ -21,17 +21,30 @@ def test_foo(u):
     pass
 
 
+v = fixture_union("v", (a, b, a))
+
+
+def test_foo2(v):
+    pass
+
+
 def test_synthesis(module_results_dct):
     if LooseVersion(pytest.__version__) < LooseVersion('3.0.0'):
         # the way to make ids uniques in case of duplicates was different in old pytest
         assert list(module_results_dct) == [
-            'test_foo[0u_is_a]',
-            'test_foo[1u_is_b]'
-            'test_foo[2u_is_a]'
+            'test_foo[1]',
+            'test_foo[2]',
+            'test_foo[3]',
+            'test_foo2[0v_is_a]',
+            'test_foo2[1v_is_b]',
+            'test_foo2[2v_is_a]'
         ]
     else:
         assert list(module_results_dct) == [
             'test_foo[1]',
             'test_foo[2]',
-            'test_foo[3]'
+            'test_foo[3]',
+            'test_foo2[v_is_a0]',
+            'test_foo2[v_is_b]',
+            'test_foo2[v_is_a1]'
         ]
