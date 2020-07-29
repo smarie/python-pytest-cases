8966ce7c13cab27ceb9aa1403f1b3291ddecdba4
Sylvain MARIE <sylvain.marie@se.com>
2020-07-29 18:02:13 +0200

    Improved test about scopes

--- pytest_cases/tests/pytest_extension/fixtures/fixture_unions/test_fixture_union_setup_teardown2.py
+++ pytest_cases/tests/pytest_extension/fixtures/fixture_unions/test_fixture_union_setup_teardown2.py
@@ -1,98 +1,136 @@
 from collections import defaultdict
-from distutils.version import LooseVersion
 
-import pytest
-
-from pytest_cases import fixture, fixture_union
-
+from pytest_cases import fixture, fixture_union, parametrize
 
 used = defaultdict(lambda: False)
 torn_down = defaultdict(lambda: False)
 
 
 @fixture(scope='session')
-def a():
+def s1():
+    name = 's1'
     global used, torn_down
-    assert not used['a']
-    used['a'] = True
-    yield 'a'
-    torn_down['a'] = True
+    assert not used[name]
+    used[name] = True
+    yield name
+    torn_down[name] += 1
+
+
+@fixture(scope='session')
+def s2():
+    name = 's2'
+    global used, torn_down
+    assert not used[name]
+    used[name] = True
+    yield name
+    torn_down[name] += 1
+
+
+@fixture(scope='session')
+def s3():
+    name = 's3'
+    global used, torn_down
+    assert not used[name]
+    used[name] = True
+    yield name
+    torn_down[name] += 1
 
 
 @fixture(scope='module')
-def b(a):
+def M1s1s2(s1, s2):
+    name = 'M1s1s2'
     global used, torn_down
-    assert not used['b']
-    used['b'] = True
-    yield 'b'
-    torn_down['b'] = True
+    assert not used[name]
+    used[name] = True
+    yield name
+    torn_down[name] += 1
+
+
+@fixture(scope='module')
+def M2s1s3(s1, s3):
+    name = 'M2s1s3'
+    global used, torn_down
+    assert not used[name]
+    used[name] = True
+    yield name
+    torn_down[name] += 1
 
 
 @fixture(scope='function')
-def c1(b):
+def F1M1s1s2(M1s1s2):
+    name = 'F1M1s1s2'
     global used, torn_down
-    used['c1'] += 1
-    yield 'c1'
-    torn_down['c1'] += 1
+    assert not used[name]
+    used[name] = True
+    yield name
+    torn_down[name] += 1
 
 
 @fixture(scope='function')
-def c2():
+@parametrize(i=[0, 1])
+def F2(i):
+    name = 'F2(%s)' % i
     global used, torn_down
-    used['c2'] += 1
-    yield 'c2'
-    torn_down['c2'] += 1
+    assert not used[name]
+    used[name] = True
+    yield name
+    torn_down[name] += 1
 
 
 @fixture(scope='function')
-def c3(a):
+def F3s2s3(s2, s3):
+    name = 'F3s2s3'
     global used, torn_down
-    used['c3'] += 1
-    yield 'c3'
-    torn_down['c3'] += 1
+    assert not used[name]
+    used[name] = True
+    yield name
+    torn_down[name] += 1
 
 
 @fixture(scope='function')
-def c4(b):
+def F4M2s1s3(M2s1s3):
+    name = 'F4M2s1s3'
     global used, torn_down
-    used['c4'] += 1
-    yield 'c4'
-    torn_down['c4'] += 1
+    assert not used[name]
+    used[name] = True
+    yield name
+    torn_down[name] += 1
 
 
-d = fixture_union('d', (c1, c2, c3, c4))
+d = fixture_union('d', (F1M1s1s2, F2, F3s2s3, F4M2s1s3))
+
+
+super_closure = None
 
 
 def test_foo(d, request):
+    # store closure for later analysis or test
+    global super_closure
     super_closure = request._pyfuncitem._fixtureinfo.names_closure
-
-    # in old pytest sorting is different and cannot be done according to scope
-    if LooseVersion(pytest.__version__) >= LooseVersion('3.5.0'):
-        assert str(super_closure) == """SuperClosure with 4 alternative closures:
- - ['d', 'a', 'b', 'c1', 'request'] (filters: d=d[0]=c1)
- - ['d', 'c2', 'request'] (filters: d=d[1]=c2)
- - ['d', 'a', 'c3', 'request'] (filters: d=d[2]=c3)
- - ['d', 'a', 'b', 'c4', 'request'] (filters: d=d[3]=c4)
-The 'super closure list' is ['a', 'b', 'd', 'c1', 'request', 'c2', 'c3', 'c4']
-
-The fixture tree is :
-(d) split: d
- -  (c1,request,b,a)
- -  (c2,request)
- -  (c3,request,a)
- -  (c4,request,b,a)
-"""
 
 
 def test_synthesis(module_results_dct):
     assert all(torn_down.values())
     assert list(module_results_dct) == [
-        'test_foo[d_is_c1]',
-        'test_foo[d_is_c4]',
-        'test_foo[d_is_c3]',
-        'test_foo[d_is_c2]',
+        'test_foo[d_is_F1M1s1s2]',
+        'test_foo[d_is_F2-i=0]',
+        'test_foo[d_is_F2-i=1]',
+        'test_foo[d_is_F3s2s3]',
+        'test_foo[d_is_F4M2s1s3]'
     ]
 
-    for item in ('a', 'b', 'c1', 'c2', 'c3', 'c4'):
-        assert used[item] == 1
-        assert torn_down[item] == 1
+    function_scoped = ('F1M1s1s2', 'F2(0)', 'F2(1)', 'F3s2s3', 'F4M2s1s3')
+    module_scoped = ('M1s1s2', 'M2s1s3')
+    session_scoped = ('s1', 's2', 's3')
+
+    for item in function_scoped + module_scoped + session_scoped:
+        assert used[item] == 1, "item %s was not used once" % item
+
+        if item in function_scoped:
+            assert torn_down[item] == 1, "item %s was not torn down once" % item
+        # else we know that the last module/session fixture alive is not properly torn down, this is a pytest issue
+
+
+# def test_super_closure():
+#     global super_closure
+#     print(super_closure)
