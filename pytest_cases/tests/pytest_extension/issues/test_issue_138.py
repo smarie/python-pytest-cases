c1bf63cc96ceb9bd16b93f23ae421cc780ab5da4
Sylvain MARIE <sylvain.marie@se.com>
2020-10-01 17:17:39 +0200

    Fixed for pytest 2

--- pytest_cases/tests/pytest_extension/issues/test_issue_138.py
+++ pytest_cases/tests/pytest_extension/issues/test_issue_138.py
@@ -1,25 +1,28 @@
 import pytest
+from pytest_cases.common_pytest_marks import has_pytest_param
+
 from pytest_cases import fixture, parametrize, fixture_ref
 
 
-@fixture
-def b():
-    print("b")
-    return "b"
+if has_pytest_param:
+    @fixture
+    def b():
+        print("b")
+        return "b"
 
 
-@parametrize("fixture", [fixture_ref(b),
-                         pytest.param(fixture_ref(b))
-                         ])
-def test(fixture):
-    assert fixture == "b"
-    print("Test ran fixure %s" % fixture)
+    @parametrize("fixture", [fixture_ref(b),
+                             pytest.param(fixture_ref(b))
+                             ])
+    def test(fixture):
+        assert fixture == "b"
+        print("Test ran fixure %s" % fixture)
 
 
-@parametrize("fixture,a", [(fixture_ref(b), 1),
-                           pytest.param(fixture_ref(b), 1)
-                           ])
-def test2(fixture, a):
-    assert fixture == "b"
-    assert a == 1
-    print("Test ran fixure %s" % fixture)
+    @parametrize("fixture,a", [(fixture_ref(b), 1),
+                               pytest.param(fixture_ref(b), 1)
+                               ])
+    def test2(fixture, a):
+        assert fixture == "b"
+        assert a == 1
+        print("Test ran fixure %s" % fixture)
