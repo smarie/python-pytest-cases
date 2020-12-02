765e136b504ec30ae4fddc1efc3d731037a0cd7b
Sylvain MARIE <sylvain.marie@se.com>
2020-12-02 12:01:34 +0100

    Fixed test for issue 148 with pytest 2

--- pytest_cases/tests/pytest_extension/issues/test_issue_148.py
+++ pytest_cases/tests/pytest_extension/issues/test_issue_148.py
@@ -1,4 +1,7 @@
+from distutils.version import LooseVersion
+
 import itertools as itt
+import pytest
 
 from pytest_cases import parametrize, fixture, param_fixtures, fixture_union
 
@@ -9,31 +12,55 @@ def mygen(name):
         yield "%s{%i}" % (name, i)
 
 
-@fixture()
-@parametrize("y", [0, 1], ids=("y{%i}" % i for i in itt.count()))
-@parametrize("x", [1, 2], ids=mygen("x"))
-def my_fixture(x, y):
-    return x, y
+if LooseVersion(pytest.__version__) < LooseVersion('3.0.0'):
+    @fixture
+    @parametrize("x", [1, 2], ids=mygen("x"))
+    def my_fixture(x):
+        return x
+
+    a = param_fixtures("a", ['aa', 'ab'], ids=mygen("a"))
+
+    my_union = fixture_union("my_union", [a], ids=mygen("myunion"))
 
 
-a = param_fixtures("a", ['aa', 'ab'], ids=mygen("a"))
+    def test_foo(my_fixture, my_union, a):
+        pass
 
 
-my_union = fixture_union("my_union", [a], ids=mygen("myunion"))
+    def test_synthesis(module_results_dct):
+        assert list(module_results_dct) == [
+            'test_foo[x{0}-myunion{0}-a{0}]',
+            'test_foo[x{0}-myunion{0}-a{1}]',
+            'test_foo[x{1}-myunion{0}-a{0}]',
+            'test_foo[x{1}-myunion{0}-a{1}]'
+        ]
+
+else:
+    @fixture
+    @parametrize("y", [0, 1], ids=("y{%i}" % i for i in itt.count()))
+    @parametrize("x", [1, 2], ids=mygen("x"))
+    def my_fixture(x, y):
+        return x, y
 
 
-def test_foo(my_fixture, my_union, a):
-    pass
+    a = param_fixtures("a", ['aa', 'ab'], ids=mygen("a"))
 
 
-def test_synthesis(module_results_dct):
-    assert list(module_results_dct) == [
-        'test_foo[x{0}-y{0}-myunion{0}-a{0}]',
-        'test_foo[x{0}-y{0}-myunion{0}-a{1}]',
-        'test_foo[x{0}-y{1}-myunion{0}-a{0}]',
-        'test_foo[x{0}-y{1}-myunion{0}-a{1}]',
-        'test_foo[x{1}-y{0}-myunion{0}-a{0}]',
-        'test_foo[x{1}-y{0}-myunion{0}-a{1}]',
-        'test_foo[x{1}-y{1}-myunion{0}-a{0}]',
-        'test_foo[x{1}-y{1}-myunion{0}-a{1}]'
-    ]
+    my_union = fixture_union("my_union", [a], ids=mygen("myunion"))
+
+
+    def test_foo(my_fixture, my_union, a):
+        pass
+
+
+    def test_synthesis(module_results_dct):
+        assert list(module_results_dct) == [
+            'test_foo[x{0}-y{0}-myunion{0}-a{0}]',
+            'test_foo[x{0}-y{0}-myunion{0}-a{1}]',
+            'test_foo[x{0}-y{1}-myunion{0}-a{0}]',
+            'test_foo[x{0}-y{1}-myunion{0}-a{1}]',
+            'test_foo[x{1}-y{0}-myunion{0}-a{0}]',
+            'test_foo[x{1}-y{0}-myunion{0}-a{1}]',
+            'test_foo[x{1}-y{1}-myunion{0}-a{0}]',
+            'test_foo[x{1}-y{1}-myunion{0}-a{1}]'
+        ]
