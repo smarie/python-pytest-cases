d337129e31fb1ed84ad5ecc5752da272ba58e4f6
Sylvain MARIE <sylvain.marie@se.com>
2020-12-08 23:41:05 +0100

    Fixed `AttributeError: 'DoctestItem' object has no attribute '_request'` when executing doctests. Fixes #156

---
+++ pytest_cases/tests/pytest_extension/issues/test_issue_doctests.py
@@ -0,0 +1,10 @@
+
+def doctestable():
+    """Do nothing, but have a doctest so that we check it will be collected and run successfully.
+
+    Examples
+    --------
+    >>> 1 + 1
+    2
+    """
+    return
