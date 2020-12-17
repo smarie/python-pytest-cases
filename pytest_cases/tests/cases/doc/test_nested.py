da8671541c9b4771329a13806898c165a72defca
Paolo Lammens <lammenspaolo@gmail.com>
2020-12-17 11:23:41 +0100

    tests: Improve test_nested

--- pytest_cases/tests/cases/doc/test_nested.py
+++ pytest_cases/tests/cases/doc/test_nested.py
@@ -9,10 +9,33 @@ class FooCases:
         def case_2(self):
             return 2
 
-    def case_3(self):
+        class SubSubFooCases:
+            def case_3(self):
+                return 3
+
+    def case_3(self):  # duplicate name; on purpose
         return 3
 
 
 @parametrize_with_cases("x", FooCases)
 def test_foo(x):
-    print(x)
+    test_foo.received.append(x)
+
+
+test_foo.received = []
+
+
+@parametrize_with_cases("x", FooCases.SubFooCases)
+def test_subfoo(x):
+    test_subfoo.received.append(x)
+
+
+test_subfoo.received = []
+
+
+def test_class_with_nested_uses_nested():
+    assert test_foo.received == [1, 2, 3, 3]
+
+
+def test_direct_nested_class_reference_works():
+    assert test_subfoo.received == [2, 3]
