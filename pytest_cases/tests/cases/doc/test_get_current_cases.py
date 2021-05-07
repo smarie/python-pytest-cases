3ad9b392a995611badece53673fa0f3de7bfb083
Sylvain MARIE <sylvain.marie@se.com>
2021-05-07 11:29:06 +0200

    Fixed builds with pytest 2

--- pytest_cases/tests/cases/doc/test_get_current_cases.py
+++ pytest_cases/tests/cases/doc/test_get_current_cases.py
@@ -5,6 +5,7 @@
 from pytest_cases import parametrize_with_cases, fixture, case, filters, get_current_cases
 from . import test_get_current_cases_cases as casesfile
 
+from pytest_cases.common_pytest_marks import PYTEST3_OR_GREATER
 
 @case(tags=("no_fix_needed",))
 def case_a():
@@ -88,79 +89,80 @@ def _assert_cases(current_cases, local=True):
     assert current_cases["withfixrefs_f2"] == current_cases["withfixrefs_f1"]
 
 
-@fixture
-@parametrize_with_cases("purelazy_t1,purelazy_t2", cases=".", prefix="tuplecase_", filter=filters.has_tags("no_fix_needed"))
-@parametrize_with_cases("withfixrefs_f1,withfixrefs_f2", cases=".", prefix="tuplecase_")
-@parametrize_with_cases("purelazy_a", cases=".", prefix="case_", filter=filters.has_tags("no_fix_needed"))
-@parametrize_with_cases("withfixrefs_f", cases=".", prefix="case_")
-def my_fixture_local(purelazy_a, purelazy_t1, purelazy_t2, withfixrefs_f, withfixrefs_f1, withfixrefs_f2, current_cases, request):
-    late_call_dct = get_current_cases(request)
-    for cases_dct in (current_cases, late_call_dct):
-        assert set(cases_dct.keys()) == {
-            "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2",
-            # NEW: the fixture
-            "my_fixture_local"
-        }
-        _assert_cases(cases_dct, local=True)
-        assert set(cases_dct["my_fixture_local"].keys()) == {
-            "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2"
-        }
-        _assert_cases(cases_dct["my_fixture_local"], local=True)
+if PYTEST3_OR_GREATER:
+    @fixture
+    @parametrize_with_cases("purelazy_t1,purelazy_t2", cases=".", prefix="tuplecase_", filter=filters.has_tags("no_fix_needed"))
+    @parametrize_with_cases("withfixrefs_f1,withfixrefs_f2", cases=".", prefix="tuplecase_")
+    @parametrize_with_cases("purelazy_a", cases=".", prefix="case_", filter=filters.has_tags("no_fix_needed"))
+    @parametrize_with_cases("withfixrefs_f", cases=".", prefix="case_")
+    def my_fixture_local(purelazy_a, purelazy_t1, purelazy_t2, withfixrefs_f, withfixrefs_f1, withfixrefs_f2, current_cases, request):
+        late_call_dct = get_current_cases(request)
+        for cases_dct in (current_cases, late_call_dct):
+            assert set(cases_dct.keys()) == {
+                "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2",
+                # NEW: the fixture
+                "my_fixture_local"
+            }
+            _assert_cases(cases_dct, local=True)
+            assert set(cases_dct["my_fixture_local"].keys()) == {
+                "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2"
+            }
+            _assert_cases(cases_dct["my_fixture_local"], local=True)
 
 
-@fixture
-@parametrize_with_cases("withfixrefs_f1,withfixrefs_f2", prefix="tuplecase_")
-@parametrize_with_cases("withfixrefs_f", prefix="case_")
-@parametrize_with_cases("purelazy_t1,purelazy_t2", prefix="tuplecase_", filter=filters.has_tags("no_fix_needed"))
-@parametrize_with_cases("purelazy_a", prefix="case_", filter=filters.has_tags("no_fix_needed"))
-def my_fixture_separate_file(purelazy_a, purelazy_t1, purelazy_t2, withfixrefs_f, withfixrefs_f1, withfixrefs_f2, current_cases, request):
-    late_call_dct = get_current_cases(request)
-    for cases_dct in (current_cases, late_call_dct):
-        assert set(cases_dct.keys()) == {
-            "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2",
-            # NEW: the fixture
-            "my_fixture_separate_file"
-        }
-        _assert_cases(cases_dct, local=False)
-        assert set(cases_dct["my_fixture_separate_file"].keys()) == {
-            "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2"
-        }
-        _assert_cases(cases_dct["my_fixture_separate_file"], local=False)
+    @fixture
+    @parametrize_with_cases("withfixrefs_f1,withfixrefs_f2", prefix="tuplecase_")
+    @parametrize_with_cases("withfixrefs_f", prefix="case_")
+    @parametrize_with_cases("purelazy_t1,purelazy_t2", prefix="tuplecase_", filter=filters.has_tags("no_fix_needed"))
+    @parametrize_with_cases("purelazy_a", prefix="case_", filter=filters.has_tags("no_fix_needed"))
+    def my_fixture_separate_file(purelazy_a, purelazy_t1, purelazy_t2, withfixrefs_f, withfixrefs_f1, withfixrefs_f2, current_cases, request):
+        late_call_dct = get_current_cases(request)
+        for cases_dct in (current_cases, late_call_dct):
+            assert set(cases_dct.keys()) == {
+                "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2",
+                # NEW: the fixture
+                "my_fixture_separate_file"
+            }
+            _assert_cases(cases_dct, local=False)
+            assert set(cases_dct["my_fixture_separate_file"].keys()) == {
+                "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2"
+            }
+            _assert_cases(cases_dct["my_fixture_separate_file"], local=False)
 
 
-@parametrize_with_cases("withfixrefs_f1,withfixrefs_f2", cases=".", prefix="tuplecase_")
-@parametrize_with_cases("withfixrefs_f", cases=".", prefix="case_")
-@parametrize_with_cases("purelazy_t1,purelazy_t2", cases=".", prefix="tuplecase_", filter=filters.has_tags("no_fix_needed"))
-@parametrize_with_cases("purelazy_a", cases=".", prefix="case_", filter=filters.has_tags("no_fix_needed"))
-def test_local_cases_with_fix(purelazy_a, purelazy_t1, purelazy_t2, withfixrefs_f, withfixrefs_f1, withfixrefs_f2, my_fixture_local, current_cases, request):
-    late_call_dct = get_current_cases(request)
-    for cases_dct in (current_cases, late_call_dct):
-        assert set(cases_dct.keys()) == {
-            "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2",
-            # NEW: the fixture
-            "my_fixture_local"
-        }
-        _assert_cases(cases_dct, local=True)
-        assert set(cases_dct["my_fixture_local"].keys()) == {
-            "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2"
-        }
-        _assert_cases(cases_dct["my_fixture_local"], local=True)
+    @parametrize_with_cases("withfixrefs_f1,withfixrefs_f2", cases=".", prefix="tuplecase_")
+    @parametrize_with_cases("withfixrefs_f", cases=".", prefix="case_")
+    @parametrize_with_cases("purelazy_t1,purelazy_t2", cases=".", prefix="tuplecase_", filter=filters.has_tags("no_fix_needed"))
+    @parametrize_with_cases("purelazy_a", cases=".", prefix="case_", filter=filters.has_tags("no_fix_needed"))
+    def test_local_cases_with_fix(purelazy_a, purelazy_t1, purelazy_t2, withfixrefs_f, withfixrefs_f1, withfixrefs_f2, my_fixture_local, current_cases, request):
+        late_call_dct = get_current_cases(request)
+        for cases_dct in (current_cases, late_call_dct):
+            assert set(cases_dct.keys()) == {
+                "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2",
+                # NEW: the fixture
+                "my_fixture_local"
+            }
+            _assert_cases(cases_dct, local=True)
+            assert set(cases_dct["my_fixture_local"].keys()) == {
+                "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2"
+            }
+            _assert_cases(cases_dct["my_fixture_local"], local=True)
 
 
-@parametrize_with_cases("withfixrefs_f1,withfixrefs_f2", prefix="tuplecase_")
-@parametrize_with_cases("withfixrefs_f", prefix="case_")
-@parametrize_with_cases("purelazy_t1,purelazy_t2", prefix="tuplecase_", filter=filters.has_tags("no_fix_needed"))
-@parametrize_with_cases("purelazy_a", prefix="case_", filter=filters.has_tags("no_fix_needed"))
-def test_separate_cases_file_with_fix(purelazy_a, purelazy_t1, purelazy_t2, withfixrefs_f, withfixrefs_f1, withfixrefs_f2, my_fixture_separate_file, current_cases, request):
-    late_call_dct = get_current_cases(request)
-    for cases_dct in (current_cases, late_call_dct):
-        assert set(cases_dct.keys()) == {
-            "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2",
-            # NEW: the fixture
-            "my_fixture_separate_file"
-        }
-        _assert_cases(cases_dct, local=False)
-        assert set(cases_dct["my_fixture_separate_file"].keys()) == {
-            "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2"
-        }
-        _assert_cases(cases_dct["my_fixture_separate_file"], local=False)
+    @parametrize_with_cases("withfixrefs_f1,withfixrefs_f2", prefix="tuplecase_")
+    @parametrize_with_cases("withfixrefs_f", prefix="case_")
+    @parametrize_with_cases("purelazy_t1,purelazy_t2", prefix="tuplecase_", filter=filters.has_tags("no_fix_needed"))
+    @parametrize_with_cases("purelazy_a", prefix="case_", filter=filters.has_tags("no_fix_needed"))
+    def test_separate_cases_file_with_fix(purelazy_a, purelazy_t1, purelazy_t2, withfixrefs_f, withfixrefs_f1, withfixrefs_f2, my_fixture_separate_file, current_cases, request):
+        late_call_dct = get_current_cases(request)
+        for cases_dct in (current_cases, late_call_dct):
+            assert set(cases_dct.keys()) == {
+                "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2",
+                # NEW: the fixture
+                "my_fixture_separate_file"
+            }
+            _assert_cases(cases_dct, local=False)
+            assert set(cases_dct["my_fixture_separate_file"].keys()) == {
+                "purelazy_a", "purelazy_t1", "purelazy_t2", "withfixrefs_f", "withfixrefs_f1", "withfixrefs_f2"
+            }
+            _assert_cases(cases_dct["my_fixture_separate_file"], local=False)
