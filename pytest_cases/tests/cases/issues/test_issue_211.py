43544fd80b7bb1f2929f26136f7aa44ff415dc40
Sylvain MARIE <sylvain.marie@se.com>
2021-05-31 17:30:33 +0200

    Improved test for issue 211

--- pytest_cases/tests/cases/issues/test_issue_211.py
+++ pytest_cases/tests/cases/issues/test_issue_211.py
@@ -18,16 +18,17 @@ class MyClassName:
 @parametrize_with_cases('val', cases=MyClassName)
 def test_function(val, current_cases, request):
     print(val)
-    case_id, case_func, case_params = current_cases['val']
-    print((case_id, case_func, case_params))
+    print(current_cases['val'])
 
-    if (case_func is MyClassName.case_widget) if PY3 else (case_func == MyClassName.case_widget):
-        if case_params['name'] == 'joe':
-            # two other ways to get the same detection :
-            assert request.node.callspec.params['widget'].argvalues[0] == "joe"
-            assert request.node.name == 'test_function[widget-name=joe]'
+    # it is a namedtuple so you can get them by position
+    val_id, val_func, val_params = current_cases['val']
 
-            pytest.skip("joe skipped")
+    if val_func == MyClassName.case_widget and val_params['name'] == 'joe':
+        # two other ways to get the same detection :
+        assert request.node.callspec.params['widget'].argvalues[0] == "joe"
+        assert request.node.name == 'test_function[widget-name=joe]'
+
+        pytest.skip("joe skipped")
 
     assert request.node.name != 'test_function[widget-name=joe]'
 
@@ -38,3 +39,4 @@ def test_synthesis(module_results_dct):
         'test_function[widget-name=alice]',
         'test_function[foo]'
     ]
+    assert module_results_dct['test_function[widget-name=joe]']['status'] == 'skipped'
