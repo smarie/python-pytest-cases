8487bbabc5227df3d32be7b14e22596387eca53d
Michele Riva <riva@iap.tuwien.ac.at>
2023-11-09 11:10:59 +0100

    Test correctness of scopes
    As suggested by @smarie

--- tests/cases/issues/issue_311/conftest.py
+++ tests/cases/issues/issue_311/conftest.py
@@ -4,4 +4,6 @@ from pytest_cases import fixture, parametrize_with_cases
 @fixture(scope='session')
 @parametrize_with_cases('arg', cases='cases', scope='session')
 def scope_mismatch(arg):
-    return arg
+    return [arg]
+
+session_scoped = scope_mismatch
